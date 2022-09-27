7c098f6f27e1e81f2158f942c2ab142f007b9c85
Griffin McLennan <mclennangriffin@gmail.com>
2022-09-27 02:34:57 -0700

    feat: setup

---
+++ pages/page1.tsx
@@ -0,0 +1,12 @@
+import { Flex, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Page1 = () => {
+  return (
+    <Flex width="100%" backgroundColor="gray.800" height="100vh">
+      <Text color="white">Page1</Text>
+    </Flex>
+  );
+};
+
+export default Page1;
---
+++ pages/page2.tsx
@@ -0,0 +1,12 @@
+import { Flex, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Page2 = () => {
+  return (
+    <Flex width="100%" backgroundColor="gray.800" height="100vh">
+      <Text color="white">Page2</Text>
+    </Flex>
+  );
+};
+
+export default Page2;
